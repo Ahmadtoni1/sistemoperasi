7252d2ff25792e1c9e449fa50ec4d0c6eaf76fba
Ahmadtoni1 <136815270+Ahmadtoni1@users.noreply.github.com>
2023-08-03 22:24:50 +0700

    latihan2.bat

---
+++ latihan2.bat
@@ -0,0 +1,21 @@
+@echo off
+title Latihan !
+rem =====Variabel======
+set P=50
+set L=60
+
+rem ======tampilan
+echo -------------------------------------
+echo Nim 		:20122055
+echo Nama 		:Ahmad Toni
+echo Kelas 		:TI 2B
+echo Jurusan		:Teknik Informatika
+echo -------------------------------------
+rem ====== rumus =========
+echo Diketahui
+set /a V= P*L
+echo panjang		:%P%
+echo lebar		:%L%
+echo -------------------------------------
+echo Luas bangunan= %V%
+pause
